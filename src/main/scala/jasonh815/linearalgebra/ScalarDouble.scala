efb65a06c1018fb5f1f0a94242d19137b754a6ce
jason <jasonhellwig@gmail.com>
2016-04-26 12:12:12 -0700

    Updated some documentation.

--- src/main/scala/jasonh815/linearalgebra/ScalarDouble.scala
+++ src/main/scala/jasonh815/linearalgebra/ScalarDouble.scala
@@ -1,7 +1,7 @@
 package jasonh815.linearalgebra
 
 /**
-  * Extends the base double class to perform operations on vetors
+  * Extends the base double class to perform operations on vectors
   */
 class ScalarDouble(val d: Double) {
   def *(vector: Vector):Vector = vector * d
--- src/main/scala/jasonh815/linearalgebra/Vector.scala
+++ src/main/scala/jasonh815/linearalgebra/Vector.scala
@@ -2,6 +2,9 @@ package jasonh815.linearalgebra
 
 import scala.language.implicitConversions
 
+/**
+  * Implementation of the mathematical concept of a vector
+  */
 class Vector(val coordinates:Seq[Double]) {
 
   if (coordinates.isEmpty) throw new InstantiationException("The coordinates must be nonempty")
@@ -14,38 +17,49 @@ class Vector(val coordinates:Seq[Double]) {
     if (!obj.isInstanceOf[Vector]) false else obj.asInstanceOf[Vector].coordinates.equals(this.coordinates)
   }
 
+  /** zips the elements of two vectors together */
   private def zipCoords(other:Vector):Seq[(Double, Double)] = {
     if (this.dimension != other.dimension) throw new IllegalArgumentException("Vectors must have the dame dimension")
     this.coordinates.zip(other.coordinates)
   }
 
+  /** add elements of two vectors */
   def plus(other:Vector):Vector = Vector(this.zipCoords(other).map(pair => pair._1 + pair._2))
 
+  /** add elements of two vectors */
   def +(other:Vector):Vector = this.plus(other)
 
+  /** subtract elements of two vectors */
   def minus(other:Vector):Vector = Vector(this.zipCoords(other).map(pair => pair._1 - pair._2))
 
+  /** subtract elements of two vectors */
   def -(other:Vector):Vector = this.minus(other)
 
+  /** multiply elements of a vector by a scalar */
   def scalarMultiply(scalar:Double):Vector = Vector(this.coordinates.map( a => a * scalar))
 
+  /** multiply elements of a vector by a scalar */
   def *(scalar:Double):Vector = this.scalarMultiply(scalar)
 }
 
 
 object Vector {
 
+  // wrapper for int and double sequences to handle type erasure and allow for overloading apply methods below
   case class DoubleSequence(seq:Seq[Double])
   case class IntSequence(seq:Seq[Int])
-
   implicit def ds(seq:Seq[Double]):DoubleSequence = DoubleSequence(seq)
   implicit def is(seq:Seq[Int]):IntSequence = IntSequence(seq)
+
+  // implicitly use sequences as vectors
   implicit def dv(seq:Seq[Double]):Vector = Vector(seq)
   implicit def iv(seq:Seq[Int]):Vector = Vector(seq)
+
+  // extend int and double primitives
   implicit def int2ScalarDouble(int: Int):ScalarDouble = new ScalarDouble(int.toDouble)
   implicit def double2ScalarDouble(double:Double):ScalarDouble = new ScalarDouble(double)
 
-
+  // vector factory methods
   def apply(x: Double*) = new Vector(x)
   def apply(x: DoubleSequence) = new Vector(x.seq)
   def apply(x: IntSequence) = new Vector(x.seq.map(_.toDouble))
